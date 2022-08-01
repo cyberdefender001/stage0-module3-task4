6fdaf25decae8d21c119cd398d5490d34bba81f9
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-01 14:19:00 +0500

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a;
-        int b;
+        int a = 3;
+        int b = 0;
         
         double sum = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
