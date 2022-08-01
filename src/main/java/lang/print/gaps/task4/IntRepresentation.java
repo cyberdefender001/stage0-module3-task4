b3f08090de9bae5d735bcd3c7aa3cd459ba56aeb
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-01 14:16:18 +0500

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c1 = 'c';
+        char c2 = 'h';
+        char c3 = 'a';
+        char c4 = 'r';
+        
+        int i1 = c1;
+        int i2 = c2;
+        int i3 = c3;
+        int i4 = c4;
     }
 }
